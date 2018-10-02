8340ba69c59a7d7eb461141d81dbd9a112c85c3e
samapriya <samapriya.roy@gmail.com>
2018-10-02 18:53:18 -0400

    added GEE tools

--- satadd/satadd.py
+++ satadd/satadd.py
@@ -10,9 +10,15 @@ from simplesearch import search
 from gbdx_mexport import mxp
 from gbdx_fpexport import fxp
 from satellogic_imagery import satfile
+from idsearch import idsearch
+from bandtypes import imgexp
+from export import exp
+from exp_report import intersect
+from os import linesep
+import textwrap as _textwrap
+from argparse import RawTextHelpFormatter
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 path=os.path.dirname(os.path.realpath(__file__))
-
 
 def dginit():
     subprocess.call('python autenticator.py', shell=True)
@@ -23,6 +29,11 @@ def satinit():
     subprocess.call('python satellogic_auth.py', shell=True)
 def satinit_from_parser(args):
     satinit()
+
+def eeinit():
+    subprocess.call('earthengine authenticate', shell=True)
+def eeinit_from_parser(args):
+    eeinit()
 
 def info_from_parser(args):
     validate()
@@ -47,48 +58,48 @@ def satraster_from_parser(args):
     satfile(sensor=args.sensor,
         geometry=args.geometry,
         target=args.local)
-# def refresh():
-#     filelist = glob.glob(os.path.join(path, "*.csv"))
-#     for f in filelist:
-#         os.remove(f)
-#     subprocess.call('python ee_rep.py', shell=True)
-#     subprocess.call('python gitcl.py', shell=True)
+
+def refresh():
+    filelist = glob.glob(os.path.join(path, "*.csv"))
+    for f in filelist:
+        os.remove(f)
+    subprocess.call('python ee_rep.py', shell=True)
+    subprocess.call('python gitcl.py', shell=True)
 
 
-
-# def refresh_from_parser(args):
-#     refresh()
-
-
-# def idsearch_from_parser(args):
-#     idsearch(mname=args.name)
-
-# def intersect_from_parser(args):
-#     intersect(start=args.start,
-#               end=args.end,
-#               geojson=args.aoi,
-#               operator=args.operator,
-#               output=args.report)
-
-# def imgexp_from_parser(args):
-#     imgexp(collection=args.id)
+def refresh_from_parser(args):
+    refresh()
 
 
-# def exp_from_parser(args):
-#     exp(
-#         collection=args.id,
-#         folderpath=args.folder,
-#         typ=args.type,
-#         start=args.start,
-#         end=args.end,
-#         bandnames=args.bandlist,
-#         geojson=args.aoi,
-#         operator=args.operator,
-#         )
+def idsearch_from_parser(args):
+    idsearch(mname=args.name)
+
+def intersect_from_parser(args):
+    intersect(start=args.start,
+              end=args.end,
+              geojson=args.aoi,
+              operator=args.operator,
+              output=args.report)
+
+def imgexp_from_parser(args):
+    imgexp(collection=args.id)
+
+
+def exp_from_parser(args):
+    exp(
+        collection=args.id,
+        folderpath=args.folder,
+        typ=args.type,
+        start=args.start,
+        end=args.end,
+        bandnames=args.bandlist,
+        geojson=args.aoi,
+        operator=args.operator,
+        )
 spacing = '                               '
 
 def main(args=None):
-    parser = argparse.ArgumentParser(description='Simple CLI for piping Planet, Satellogic & GBDX Assets')
+    parser = argparse.ArgumentParser(description='Simple CLI for piping Planet, Satellogic & GBDX Assets', formatter_class=argparse.RawTextHelpFormatter)
 
     subparsers = parser.add_subparsers()
     parser_dginit = subparsers.add_parser('dginit',help='Initialize GBDX')
@@ -97,8 +108,10 @@ def main(args=None):
     parser_satinit = subparsers.add_parser('satinit',help='Initialize Satellogic Tokens')
     parser_satinit.set_defaults(func=satinit_from_parser)
 
-    parser_info = subparsers.add_parser('info',help='Prints account info for GBDX')
-    parser_info.set_defaults(func=info_from_parser)
+    parser_eeinit = subparsers.add_parser('eeinit',help='''Initialize Google Earth Engine
+
+        ''')
+    parser_eeinit.set_defaults(func=eeinit_from_parser)
 
     parser_simple_search = subparsers.add_parser('simple_search',help='Simple search to look for DG assets that intersect your AOI handles KML/SHP/GEOJSON')
     parser_simple_search.add_argument('--local',help='full path for folder or file with SHP/KML/GEOJSON')
@@ -107,27 +120,66 @@ def main(args=None):
     parser_simple_search.add_argument('--limit',help='Limit the number of items to search')
     parser_simple_search.set_defaults(func=simple_search_from_parser)
 
-    parser_metadata = subparsers.add_parser('metadata',help='Exports metadata for simple search into constitudent folders as JSON files')
+    parser_metadata = subparsers.add_parser('metadata',help='''Exports metadata for simple search into constitutent folders as JSON files''')
     parser_metadata.add_argument('--local',help='full path for folder or file with SHP/KML/GEOJSON')
     parser_metadata.add_argument('--start',help='start date YYYY-MM-DD')
     parser_metadata.add_argument('--end',help='end date YYYY-MM-DD')
     parser_metadata.add_argument('--limit',help='Limit the number of items to search')
     parser_metadata.set_defaults(func=metadata_from_parser)
 
-    parser_footprint = subparsers.add_parser('footprint',help='Exports footprint for metadata files extracted earlier and converts them to indidual geometries (GeoJSON) and combined geomtry (GeoJSON) file')
+    parser_footprint = subparsers.add_parser('footprint',help='''Exports footprint for metadata files extracted earlier'''+
+    linesep+"and converts them to individual geometries (GeoJSON)and combined geomtry (GeoJSON) file"+'''
+
+    ''')
     parser_footprint.add_argument('--local',help='full path for folder with metadata JSON files')
     parser_footprint.add_argument('--dirc',help='directory to store individual geometries')
-    parser_footprint.add_argument('--output',help='path to combined footprint geometry geojson')
+    parser_footprint.add_argument('--output',help="path to combined footprint geometry geojson")
     parser_footprint.set_defaults(func=footprint_from_parser)
 
-    parser_satraster = subparsers.add_parser('satraster',help='Filter and download Satellogic Imagery')
+    parser_satraster = subparsers.add_parser('satraster',help='''Filter and download Satellogic Imagery
+
+        ''')
     parser_satraster.add_argument('--sensor',help='Choose micro or macro depending on multispec or hyperspectal')
     parser_satraster.add_argument('--local',help='Local Path to save files')
     optional_named = parser_satraster.add_argument_group('Optional named arguments')
-    optional_named.add_argument('--geometry',help='Pass GeoJSON geometry file as filter')
-    parser_satraster.set_defaults(func=satraster_from_parser)
-    args = parser.parse_args()
+    optional_named.add_argument('--geometry',help='''Pass GeoJSON geometry file as filter
 
+        ''')
+    parser_satraster.set_defaults(func=satraster_from_parser)
+
+    parser_refresh = subparsers.add_parser('refresh',help='Refreshes your personal asset list and GEE Asset list')
+    parser_refresh.set_defaults(func=refresh_from_parser)
+    parser_idsearch = subparsers.add_parser('idsearch',help='Does possible matches using asset name to give you asseth id/full path')
+    parser_idsearch.add_argument('--name',help='Name or part of name to search for')
+    parser_idsearch.set_defaults(func=idsearch_from_parser)
+
+
+    parser_intersect = subparsers.add_parser('intersect',help='Exports a report of all assets(Personal & GEE) intersecting with provided geometry')
+    parser_intersect.add_argument('--start',help='Start date to filter image',default=None)
+    parser_intersect.add_argument('--end', help='End date to filter image', default=None)
+    parser_intersect.add_argument('--aoi',help='Full path to geojson/json/kml to be used for bounds')
+    parser_intersect.add_argument('--report',help='Full path where the report will be exported including type, path & number of intersects', default=None)
+    optional_named = parser_intersect.add_argument_group('Optional named arguments for geometry only')
+    optional_named.add_argument('--operator',help='Use bb for Bounding box incase the geometry is complex or has too many vertices', default=None)
+    parser_intersect.set_defaults(func=intersect_from_parser)
+
+    parser_imgexp = subparsers.add_parser('bandtype',help='Prints GEE bandtype and generates list to be used for export')
+    parser_imgexp.add_argument('--id',help='full path for collection or image')
+    parser_imgexp.set_defaults(func=imgexp_from_parser)
+
+    parser_exp = subparsers.add_parser('export',help='Export GEE Collections based on filter')
+    parser_exp.add_argument('--id',help='Full path for collection or image')
+    parser_exp.add_argument('--folder', help='Drive folder path')
+    parser_exp.add_argument('--type',help='Type whether image or collection')
+    parser_exp.add_argument('--aoi',help='Full path to geojson/json/kml to be used for bounds')
+    optional_named = parser_exp.add_argument_group('Optional named arguments for image collection only')
+    optional_named.add_argument('--start',help='Start date to filter image',default=None)
+    optional_named.add_argument('--end', help='End date to filter image', default=None)
+    optional_named.add_argument('--bandlist',help='Bandlist we generated from bandtype export must be same bandtype', default=None)
+    optional_named.add_argument('--operator',help='Use bb for Bounding box incase the geometry is complex or has too many vertices', default=None)
+    parser_exp.set_defaults(func=exp_from_parser)
+
+    args = parser.parse_args()
     args.func(args)
 
 
